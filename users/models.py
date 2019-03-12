1e6b244a1852a68fbdfbcdfa224db210b61ba721
Jeff Rojas <jrojas3991@gmail.com>
2019-03-12 16:27:27 -0400

    added signup form and login info

--- users/models.py
+++ users/models.py
@@ -3,4 +3,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 class User(AbstractBaseUser):
-    phone_number = models.CharField()
+    phone_number = models.CharField(max_length=10)
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField()
+    name = models.CharField(max_length=255)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['phone_number', 'date_of_birth', 'name']
--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth import get_user_model, authenticate, login
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .forms import SignupForm
+
+
+User = get_user_model()
+
+class SignupView(CreateView):
+    template_name = 'signup.html'
+    model = User
+    form_class = SignupForm
+    success_url = reverse_lazy('rides:ride_list')
+
+    def form_valid(self, form):
+        response = super(form).form_valid(form)
+
+        # Authenticate the user checks provided password against the hash
+        user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password1'])
+
+        # Login the user (does the session table/cookie stuff)
+        login(self.request, user)
+
+        return response
+
