52cdd54bf7a2f93155eb6b8fc818ee5102a8e426
Jeff Rojas <jrojas3991@gmail.com>
2019-03-28 17:32:49 -0400

    updated table

--- frontend/static/src/components/App.js
+++ frontend/static/src/components/App.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 
 import Alert from './Alerts'
 
-import {Table} from "react-bootstrap";
+import {Table, Nav, Navbar, Form, FormControl, Button} from "react-bootstrap";
 
-import { Nav } from"react-bootstrap";
-
-import { Navbar } from"react-bootstrap";
-
-import { Form } from"react-bootstrap";
-
-import { FormControl } from"react-bootstrap";
-
-import { Button } from"react-bootstrap";
+// import {Nav} from "react-bootstrap";
+//
+// import {Navbar} from "react-bootstrap";
+//
+// import {Form} from "react-bootstrap";
+//
+// import {FormControl} from "react-bootstrap";
+//
+// import {Button} from "react-bootstrap";
 
 
 import '../App.css';
@@ -47,10 +47,10 @@ class App extends Component {
         }
     }
 
-    setAlert(alertData){
+    setAlert(alertData) {
         console.log('alert data', alertData);
 
-        let headers ={
+        let headers = {
             "Content-Type": "application/json"
         };
 
@@ -68,63 +68,63 @@ class App extends Component {
 
 
     render() {
+        let rows = this.state.data.map((market) => {
+
+            return (
+                <tr>
+                    <td>{market.symbol}</td>
+                    <td>{market.name}</td>
+                    <td>{market.quote.USD.price}</td>
+                    <td>{market.circulating_supply}</td>
+                    <td>{market.quote.USD.percent_change_1h}</td>
+                    <td>{market.quote.USD.percent_change_24h}</td>
+                    <td>{market.quote.USD.percent_change_7d}</td>
+                    <td><Alert currencyId={market.id} setAlert={this.setAlert}/></td>
+                </tr>
+
+            )
+        });
+
         return (
             <div className="App">
                 {/*<h1 className={'heading'}>Coin Tracker</h1>*/}
                 <div className={'backdrop'}>
-                      <Navbar bg="light" variant="light">
+                    <Navbar bg="light" variant="light">
                         <Navbar.Brand href="#home">Coin Tracker</Navbar.Brand>
                         <Nav className="mr-auto">
-                          <Nav.Link href="#home">Home</Nav.Link>
-                          <Nav.Link href="#features">Alerts</Nav.Link>
-                          <Nav.Link href="#pricing">Log Out</Nav.Link>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Alerts</Nav.Link>
+                            <Nav.Link href="#pricing">Log Out</Nav.Link>
                         </Nav>
                         <Form inline>
-                          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                          <Button variant="outline-primary">Search</Button>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                            <Button variant="outline-primary">Search</Button>
                         </Form>
-                      </Navbar>
-                    <h2 className={'username'}>Jeff Rojas</h2>
-               </div>
+                    </Navbar>
+
+                </div>
+
 
                 <div className={"crypto-data col-md-8"}>
-                    {this.state.data.map((market) => {
-                        // console.log('market', market.id)
-                        return (
 
+                    <Table size="sm" className="table-striped">
+                        <thead>
+                        <tr>
+                            <th>Symbol</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Circulating Supply</th>
+                            <th>% Change 1h</th>
+                            <th>% Change 24h</th>
+                            <th>% Change 7d</th>
+                            <th></th>
+                        </tr>
+                        </thead>
 
-
-                            <Table size="sm" key={market.id}>
-                                <thead>
-                                <tr>
-                                    <th>Symbol</th>
-                                    <th>Name</th>
-                                    <th>Price</th>
-                                    <th>Circulating Supply</th>
-                                    <th>% Change 1h</th>
-                                    <th>% Change 24h</th>
-                                    <th>% Change 7d</th>
-                                    <th></th>
-                                </tr>
-                                </thead>
-
-                                <tbody>
-                                <tr>
-
-                                    <td>{market.symbol}</td>
-                                    <td>{market.name}</td>
-                                    <td>{market.quote.USD.price}</td>
-                                    <td>{market.circulating_supply}</td>
-                                    <td>{market.quote.USD.percent_change_1h}</td>
-                                    <td>{market.quote.USD.percent_change_24h}</td>
-                                    <td>{market.quote.USD.percent_change_7d}</td>
-                                    <td><Alert currencyId={market.id} setAlert={this.setAlert}/></td>
-                                </tr>
-                                </tbody>
-                            </Table>
-
-                        )
-                    })}
+                        <tbody>
+                        {rows}
+                        </tbody>
+                    </Table>
                 </div>
 
 
